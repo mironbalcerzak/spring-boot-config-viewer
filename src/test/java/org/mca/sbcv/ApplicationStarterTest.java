8f507eb73caf673424774f068a92e1558ce42af6
Miron Balcerzak <mironbalcerzak@gmail.com>
2024-01-25 22:49:30 +0100

    Spring Initializr commit

---
+++ src/test/java/org/mca/sbcv/ApplicationStarterTest.java
@@ -0,0 +1,13 @@
+package org.mca.sbcv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationStarterTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
